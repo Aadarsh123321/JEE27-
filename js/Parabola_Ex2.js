8ebd298bd224149567f5c26a19f1f06ee530bd2a
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:12:33 +0530

    Create Parabola_Ex2.js

---
+++ js/Parabola_Ex2.js
@@ -0,0 +1,5 @@
+// js/Parabola_Ex2.js
+window.customQData = [
+  { id: 1, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278866/question_page_299_1786268650927_irh20u.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1, 2, 3], s: "Detailed solution will be updated soon." }
+];
+
