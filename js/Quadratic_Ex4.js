1881bf090a6f7d7dbb58c0fc5a416afe41deefda
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:27:13 +0530

    Create Quadratic_Ex4.js

---
+++ js/Quadratic_Ex4.js
@@ -0,0 +1,8 @@
+// js/Quadratic_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786223879/question_page_171_1786217541973_mw2re3.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QPRS", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786223879/question_page_171_1786217546545_cjdjjt.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QSRP", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786223880/question_page_171_1786217550829_u4gg7j.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRPS", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786223881/question_page_172_1786217706666_wwzkdq.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QPSR", s: "Detailed solution will be updated soon." }
+];
+
