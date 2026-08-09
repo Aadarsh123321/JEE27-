1afd58ad1e5a761c087df3d7faf52eb9fdf222de
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:06:06 +0530

    Create StraightLines_Ex3.js

---
+++ js/StraightLines_Ex3.js
@@ -0,0 +1,8 @@
+// js/StraightLines_Ex3.js
+window.customQData = [
+  { id: 1, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277310/question_page_278_1786267692264_mofkby.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 3, s: "Detailed solution will be updated soon." },
+  { id: 2, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277310/question_page_278_1786267694812_qucitp.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 0, s: "Detailed solution will be updated soon." },
+  { id: 3, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277316/question_page_278_1786267698464_iwqkk4.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 1, s: "Detailed solution will be updated soon." },
+  { id: 4, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277316/question_page_278_1786267700769_lbsm70.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 3, s: "Detailed solution will be updated soon." }
+];
+
