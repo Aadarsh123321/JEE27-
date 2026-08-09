c9d90c36b5907ed5bb1d130670cdb572f5d0c7c9
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:21:15 +0530

    Create TrigEquations_Ex4.js

---
+++ js/TrigEquations_Ex4.js
@@ -0,0 +1,7 @@
+// js/TrigEquations_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285481/question_page_340_1786270335200_govjel.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "R,T; P; P,Q,R; Q", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285483/question_page_340_1786270339090_wqkh67.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "S; P,T; R; T", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285485/question_page_340_1786270341674_qqrj2p.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SQPR", s: "Detailed solution will be updated soon." }
+];
+
