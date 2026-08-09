1c925a67093853930df9b615899b39b9011b7b7c
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:15:19 +0530

    Create Ellipse_Ex4.js

---
+++ js/Ellipse_Ex4.js
@@ -0,0 +1,5 @@
+// js/Ellipse_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279238/question_page_306_1786268788836_yl406r.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "P,R; R,S; P; Q", s: "Detailed solution will be updated soon." }
+];
+
