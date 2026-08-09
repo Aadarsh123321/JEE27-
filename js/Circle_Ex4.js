7a8119980167ab369debd0b8e4093e6be5414bef
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:11:00 +0530

    Create Circle_Ex4.js

---
+++ js/Circle_Ex4.js
@@ -0,0 +1,6 @@
+// js/Circle_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277914/question_page_293_1786268339792_o2isgu.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QSSS", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277916/question_page_293_1786268344965_rlcckz.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PQRS", s: "Detailed solution will be updated soon." }
+];
+
