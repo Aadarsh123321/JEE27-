691bb780536ef9b921b5edab945cd66a57199d1f
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:15:29 +0530

    Create Ellipse_Ex5.js

---
+++ js/Ellipse_Ex5.js
@@ -0,0 +1,6 @@
+// js/Ellipse_Ex5.js
+window.customQData = [
+  { id: 1, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279239/question_page_307_1786268798935_w7lpbd.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "4", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279244/question_page_307_1786268803475_ifpv7d.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "0", s: "Detailed solution will be updated soon." }
+];
+
