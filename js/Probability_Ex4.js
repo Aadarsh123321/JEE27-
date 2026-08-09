7063748e64b5a19121927bc49f793747bb6bfe8e
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:56:56 +0530

    Create Probability_Ex4.js

---
+++ js/Probability_Ex4.js
@@ -0,0 +1,5 @@
+// js/Probability_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786251506/question_page_251_1786221669205_tlpok6.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PQSS", s: "Detailed solution will be updated soon." }
+];
+
