86de29954e8d1baf220c9b78eb3cbd4b8fb0d3d1
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:27:11 +0530

    Create Vector3D_Ex4.js

---
+++ js/Vector3D_Ex4.js
@@ -0,0 +1,7 @@
+// js/Vector3D_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286911/question_page_384_1786272324945_dig1af.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "Q,S; R; P,Q; P", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286915/question_page_384_1786272329460_k6dytd.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "R; P; S; Q", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286918/question_page_385_1786272334311_j9dhlp.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "P,R; P,Q,S; P,Q,R,S; P,T", s: "Detailed solution will be updated soon." }
+];
+
