4660b9bf50de664b57fb15241f858ea3653559f3
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:08:20 +0530

    Create StraightLines_Ex4.js

---
+++ js/StraightLines_Ex4.js
@@ -0,0 +1,7 @@
+// js/StraightLines_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277336/question_page_279_1786267709262_nhgjqf.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RQSP", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277337/question_page_279_1786267715907_rm4bke.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786277338/question_page_279_1786267792306_lkor2s.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." }
+];
+
