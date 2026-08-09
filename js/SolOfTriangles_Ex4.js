f2cca7f7451bded160245860859b4e263a8f4abc
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:23:05 +0530

    Create SolOfTriangles_Ex4.js

---
+++ js/SolOfTriangles_Ex4.js
@@ -0,0 +1,8 @@
+// js/SolOfTriangles_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285940/question_page_355_1786271149992_fcosn6.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PPTS", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285942/question_page_355_1786271155154_vpz54f.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SPRQ", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285944/question_page_355_1786271320552_kldo1t.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RSTP", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285948/question_page_356_1786271362066_l9irmk.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SPQR", s: "Detailed solution will be updated soon." }
+];
+
