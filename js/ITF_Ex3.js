4c8eb9605709851a8ade870928f2803b0fb0fe6a
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:24:42 +0530

    Create ITF_Ex3.js

---
+++ js/ITF_Ex3.js
@@ -0,0 +1,6 @@
+// js/ITF_Ex3.js
+window.customQData = [
+  { id: 1, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286336/question_page_366_1786271774321_fxtwar.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 0, s: "Detailed solution will be updated soon." },
+  { id: 2, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286338/question_page_366_1786271779252_ohkekg.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 3, s: "Detailed solution will be updated soon." }
+];
+
