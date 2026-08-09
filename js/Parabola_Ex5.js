e25de30e74980795f36c46e9cdf6b76a777e179f
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:13:19 +0530

    Create Parabola_Ex5.js

---
+++ js/Parabola_Ex5.js
@@ -0,0 +1,7 @@
+// js/Parabola_Ex5.js
+window.customQData = [
+  { id: 1, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278955/question_page_302_1786268689343_lxmoov.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "8", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278961/question_page_302_1786268692955_dfprpp.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "7", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278962/question_page_302_1786268698779_tedh7k.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "1", s: "Detailed solution will be updated soon." }
+];
+
