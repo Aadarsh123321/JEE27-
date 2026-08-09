eee1aa86b1224826b15d5490bc4b7b3e690d032a
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:12:54 +0530

    Create Parabola_Ex3.js

---
+++ js/Parabola_Ex3.js
@@ -0,0 +1,7 @@
+// js/Parabola_Ex3.js
+window.customQData = [
+  { id: 1, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278903/question_page_300_1786268661432_fx6kou.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 3, s: "Detailed solution will be updated soon." },
+  { id: 2, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278917/question_page_300_1786268665384_vdamql.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 1, s: "Detailed solution will be updated soon." },
+  { id: 3, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278923/question_page_300_1786268667614_wferat.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 2, s: "Detailed solution will be updated soon." }
+];
+
