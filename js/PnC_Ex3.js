cd915cf5b93cbd4df40220091b86f16bb180486b
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:38:06 +0530

    Create PnC_Ex3.js

---
+++ js/PnC_Ex3.js
@@ -0,0 +1,6 @@
+// js/PnC_Ex3.js
+window.customQData = [
+  { id: 1, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786250726/question_page_233_1786220456490_q6hhup.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 3, s: "Detailed solution will be updated soon." },
+  { id: 2, type: "single", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786250727/question_page_233_1786220454429_xvt1zs.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: 2, s: "Detailed solution will be updated soon." }
+];
+
