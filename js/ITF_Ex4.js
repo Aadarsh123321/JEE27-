7563660312be276e0f1624b6c9f7125a3d2afac4
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:25:28 +0530

    Create ITF_Ex4.js

---
+++ js/ITF_Ex4.js
@@ -0,0 +1,8 @@
+// js/ITF_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286343/question_page_367_1786271815359_pnealr.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QSPR", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286345/question_page_367_1786271819077_qkzyfs.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PQRS", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286350/question_page_368_1786271825130_ezm2zd.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SRPQ", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286352/question_page_368_1786271828728_wlsnb6.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RPQS", s: "Detailed solution will be updated soon." }
+];
+
