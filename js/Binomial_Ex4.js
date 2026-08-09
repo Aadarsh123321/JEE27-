bb20de9acaa6a4c1e4b3cd3ea4384244548e5409
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:41:05 +0530

    Create Binomial_Ex4.js

---
+++ js/Binomial_Ex4.js
@@ -0,0 +1,7 @@
+// js/Binomial_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786251091/question_page_241_1786221106644_dfdcwz.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "Q; S; R; P,Q,R,S,T", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786251091/question_page_241_1786221110396_ir4jmw.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "S; Q; P; R", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786251093/question_page_241_1786221285538_xuunbh.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." }
+];
+
