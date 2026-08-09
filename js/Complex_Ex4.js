2034f00c1389ca247362ac50ea8af09ffa75d783
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:34:14 +0530

    Create Complex_Ex4.js

---
+++ js/Complex_Ex4.js
@@ -0,0 +1,7 @@
+// js/Complex_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786225783/question_page_217_1786219694321_sfn3lt.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "S,T,S,QRST", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786225784/question_page_217_1786219698166_eq1ibv.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RSQP", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786225789/question_page_218_1786219725356_awyiss.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SRQP", s: "Detailed solution will be updated soon." }
+];
+
