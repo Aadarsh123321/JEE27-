fd7dc36b30554150f331e3fc4793867e399da09a
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:36:09 +0530

    Create Matrices_Ex4.js

---
+++ js/Matrices_Ex4.js
@@ -0,0 +1,9 @@
+// js/Matrices_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226623/question_page_224_1786219923689_wakjmc.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "P,Q,T; S; P,R; R", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226631/question_page_224_1786219926886_qc2lfa.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "R; P,Q,S; P,R; P,Q,R,S", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226634/question_page_225_1786220083310_ndq19t.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226644/question_page_225_1786220117006_difpcu.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SRPQ", s: "Detailed solution will be updated soon." },
+  { id: 5, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226646/question_page_225_1786220127406_jgf7gu.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QSPR", s: "Detailed solution will be updated soon." }
+];
+
