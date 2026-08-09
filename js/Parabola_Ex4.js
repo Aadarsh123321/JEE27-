c9af451feb72a19ec1e1068c51683ded5c8c35f3
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:13:07 +0530

    Create Parabola_Ex4.js

---
+++ js/Parabola_Ex4.js
@@ -0,0 +1,6 @@
+// js/Parabola_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278952/question_page_301_1786268677571_xsdmyh.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SPQR", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786278953/question_page_301_1786268681443_uhamql.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PQST", s: "Detailed solution will be updated soon." }
+];
+
