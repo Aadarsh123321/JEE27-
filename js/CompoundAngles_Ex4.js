3c86f43ab6bfbd2d29e36ac69bfa7dac2175a210
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:19:32 +0530

    Create CompoundAngles_Ex4.js

---
+++ js/CompoundAngles_Ex4.js
@@ -0,0 +1,9 @@
+// js/CompoundAngles_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285003/question_page_330_1786269773167_hyjmwa.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RQSP", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285004/question_page_330_1786269776692_rhzzr5.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285008/question_page_330_1786269932784_faqlif.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRSP", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285011/question_page_331_1786269994331_r3ik99.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "R,T; P; P,Q,R; Q", s: "Detailed solution will be updated soon." },
+  { id: 5, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285013/question_page_331_1786269998016_b1jusj.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRPS", s: "Detailed solution will be updated soon." }
+];
+
