8367f5d9c3940a0f1791a6eda06cee55c276cf0d
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:36:22 +0530

    Create Matrices_Ex5.js

---
+++ js/Matrices_Ex5.js
@@ -0,0 +1,9 @@
+// js/Matrices_Ex5.js
+window.customQData = [
+  { id: 1, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226653/question_page_227_1786220231022_jzcdgh.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "3", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226658/question_page_227_1786220235822_mrerri.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "7", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226661/question_page_227_1786220239207_oc6kga.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "0", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226666/question_page_227_1786220242956_y5ofiz.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "4", s: "Detailed solution will be updated soon." },
+  { id: 5, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786226670/question_page_227_1786220246393_dbweur.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "5", s: "Detailed solution will be updated soon." }
+];
+
