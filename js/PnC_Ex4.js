b00e170f349719b25bbc185b453cada0910018f7
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:38:33 +0530

    Create PnC_Ex4.js

---
+++ js/PnC_Ex4.js
@@ -0,0 +1,6 @@
+// js/PnC_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786250756/question_page_234_1786220466394_f725a6.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "TQRP", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786250756/question_page_234_1786220470594_wq7ujw.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QRPT", s: "Detailed solution will be updated soon." }
+];
+
