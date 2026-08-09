dce082324cba8d779d53ba23ad76140af1c6c073
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:24:24 +0530

    Create ITF_Ex2.js

---
+++ js/ITF_Ex2.js
@@ -0,0 +1,10 @@
+// js/ITF_Ex2.js
+window.customQData = [
+  { id: 1, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286314/question_page_365_1786271739478_g5a0ov.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1, 2], s: "Detailed solution will be updated soon." },
+  { id: 2, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286318/question_page_365_1786271742288_pberbd.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 2], s: "Detailed solution will be updated soon." },
+  { id: 3, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286322/question_page_365_1786271745799_e2u3ur.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1, 2], s: "Detailed solution will be updated soon." },
+  { id: 4, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286324/question_page_365_1786271751696_tidboy.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1], s: "Detailed solution will be updated soon." },
+  { id: 5, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286329/question_page_365_1786271758029_pzbask.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1, 3], s: "Detailed solution will be updated soon." },
+  { id: 6, type: "multi", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786286332/question_page_365_1786271762352_sqv9kp.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: ["Option A", "Option B", "Option C", "Option D"], c: [0, 1, 3], s: "Detailed solution will be updated soon." }
+];
+
