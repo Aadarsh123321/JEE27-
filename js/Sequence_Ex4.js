20406bd7fa5e14bd9543cf2ac5c303589ec17f80
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-09 23:30:07 +0530

    Create Sequence_Ex4.js

---
+++ js/Sequence_Ex4.js
@@ -0,0 +1,10 @@
+// js/Sequence_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224842/question_page_192_1786218725300_kg0v7a.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RPQS", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224847/question_page_192_1786218728657_cxblxu.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RRPR", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224866/question_page_192_1786218731668_omjizf.jpg" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "PRSQ", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224868/question_page_193_1786218846675_espskq.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "QPTS", s: "Detailed solution will be updated soon." },
+  { id: 5, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224870/question_page_193_1786218850614_cmyggf.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SRPQ", s: "Detailed solution will be updated soon." },
+  { id: 6, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786224876/question_page_194_1786218857623_u7n8e8.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "SPQR", s: "Detailed solution will be updated soon." }
+];
+
