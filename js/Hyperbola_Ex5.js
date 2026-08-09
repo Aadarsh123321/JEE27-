45a2f9db458bf98016bf5957451b15186f818d61
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:17:47 +0530

    Create Hyperbola_Ex5.js

---
+++ js/Hyperbola_Ex5.js
@@ -0,0 +1,7 @@
+// js/Hyperbola_Ex5.js
+window.customQData = [
+  { id: 1, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279494/question_page_312_1786268923496_opijhg.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "8", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279499/question_page_312_1786268928571_udnhad.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "4", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786279504/question_page_312_1786268932017_lfayg1.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "4", s: "Detailed solution will be updated soon." }
+];
+
