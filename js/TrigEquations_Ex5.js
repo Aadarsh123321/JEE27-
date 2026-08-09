96f3a357ba9e038bea3c40a18aff41dc58b2da1a
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:21:26 +0530

    Create TrigEquations_Ex5.js

---
+++ js/TrigEquations_Ex5.js
@@ -0,0 +1,13 @@
+// js/TrigEquations_Ex5.js
+window.customQData = [
+  { id: 1, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285487/question_page_342_1786270435402_maglk5.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "5", s: "Detailed solution will be updated soon." },
+  { id: 2, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285491/question_page_342_1786270438449_qnmcq0.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "9", s: "Detailed solution will be updated soon." },
+  { id: 3, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285495/question_page_342_1786270442661_zfe4rv.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "6", s: "Detailed solution will be updated soon." },
+  { id: 4, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285498/question_page_342_1786270446920_hypktg.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "7", s: "Detailed solution will be updated soon." },
+  { id: 5, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285503/question_page_342_1786270452716_vwg17p.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "3", s: "Detailed solution will be updated soon." },
+  { id: 6, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285504/question_page_342_1786270456979_b9paoh.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "8", s: "Detailed solution will be updated soon." },
+  { id: 7, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285509/question_page_342_1786270461757_gorkxa.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "5", s: "Detailed solution will be updated soon." },
+  { id: 8, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285513/question_page_342_1786270465594_q2ctyt.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "8", s: "Detailed solution will be updated soon." },
+  { id: 9, type: "numeric", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786285517/question_page_342_1786270469426_vrmwxm.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "8", s: "Detailed solution will be updated soon." }
+];
+
