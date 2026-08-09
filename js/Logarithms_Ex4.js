8c6ddc2b99af2e0bffbf8e12c08b9c263396ab98
Aadarsh123321 <134625365+Aadarsh123321@users.noreply.github.com>
2026-08-10 00:00:58 +0530

    Create Logarithms_Ex4.js

---
+++ js/Logarithms_Ex4.js
@@ -0,0 +1,5 @@
+// js/Logarithms_Ex4.js
+window.customQData = [
+  { id: 1, type: "match", t: '<img src="https://res.cloudinary.com/yhelbo6v/image/upload/v1786252101/question_page_264_1786222109321_ufo6n5.png" style="width: 100%; border-radius: 8px;" alt="Q">', o: [], c: "RSPP", s: "Detailed solution will be updated soon." }
+];
+
